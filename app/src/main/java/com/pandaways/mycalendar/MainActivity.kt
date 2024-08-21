208c77abfc0b282b947fec1288785cb6fd1d7abf
abdullahalhakimi <abdullahalhakimi7@gmail.com>
2024-08-21 11:43:44 +0300

    Improve Design && Trigger an Action Based on the Selected Date

--- app/src/main/java/com/pandaways/mycalendar/MainActivity.kt
+++ app/src/main/java/com/pandaways/mycalendar/MainActivity.kt
@@ -3,22 +3,36 @@ package com.pandaways.mycalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.pandaways.mycalendar.ui.CalendarApp
 import com.pandaways.mycalendar.ui.theme.MyCalendarTheme
 
-
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyCalendarTheme {
-                CalendarApp(
-                    modifier = Modifier.padding(16.dp)
-                )
+                Card(
+                    modifier = Modifier
+                        .padding(vertical = 10.dp, horizontal = 15.dp),
+                ) {
+                    CalendarApp(
+                        modifier = Modifier
+                            .background(color = MaterialTheme.colorScheme.surfaceVariant)
+                            .padding(horizontal = 15.dp),
+                        onSelectedDataChange = { newData ->
+                            // Trigger an Action Based on the Selected Date
+                        }
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/pandaways/mycalendar/ui/CalendarApp.kt
+++ app/src/main/java/com/pandaways/mycalendar/ui/CalendarApp.kt
@@ -1,16 +1,30 @@
 package com.pandaways.mycalendar.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronLeft
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +42,8 @@ import java.time.format.FormatStyle
 fun CalendarAppPreview() {
     MyCalendarTheme {
         CalendarApp(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            onSelectedDataChange = {},
         )
     }
 }
@@ -36,19 +51,28 @@ fun CalendarAppPreview() {
 @Composable
 fun CalendarApp(
     modifier: Modifier = Modifier,
+    onSelectedDataChange: (CalendarUiModel.Date) -> Unit,
 ) {
     val dataSource = CalendarDataSource()
     var data by remember { mutableStateOf(dataSource.getData(lastSelectedDate = dataSource.today)) }
-    Column(modifier = modifier.fillMaxSize()) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
         Header(
             data = data,
             onPrevClickListener = { startDate ->
                 val finalStartDate = startDate.minusDays(1)
-                data = dataSource.getData(startDate = finalStartDate, lastSelectedDate = data.selectedDate.date)
+                data = dataSource.getData(
+                    startDate = finalStartDate,
+                    lastSelectedDate = data.selectedDate.date
+                )
             },
             onNextClickListener = { endDate ->
                 val finalStartDate = endDate.plusDays(2)
-                data = dataSource.getData(startDate = finalStartDate, lastSelectedDate = data.selectedDate.date)
+                data = dataSource.getData(
+                    startDate = finalStartDate,
+                    lastSelectedDate = data.selectedDate.date
+                )
             }
         )
         Content(data = data) { date ->
@@ -61,6 +85,7 @@ fun CalendarApp(
                 }
             )
         }
+        Spacer(modifier = Modifier.height(20.dp))
     }
 }
 
@@ -128,8 +153,7 @@ fun ContentItem(
             .padding(vertical = 4.dp, horizontal = 4.dp)
             .clickable {
                 onClickListener(date)
-            }
-        ,
+            },
         colors = CardDefaults.cardColors(
             containerColor = if (date.isSelected) {
                 MaterialTheme.colorScheme.primary
